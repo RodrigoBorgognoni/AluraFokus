f80f3e0f7bdd2b71309f73a9127fc39a89b67af3
Rodrigo Borgognoni <borgognonirodrigo@gmail.com>
2024-09-16 21:27:30 -0300

    Finalizado

--- js/main.js
+++ js/main.js
@@ -2,20 +2,74 @@ const html = document.querySelector("html");
 const focoBtn = document.querySelector(".app__card-button--foco");
 const curtoBtn = document.querySelector(".app__card-button--curto");
 const longoBtn = document.querySelector(".app__card-button--longo");
+const foto = document.querySelector(".app__image");
 const titulo = document.querySelector(".app__title");
 const botoes = document.querySelectorAll(".app__card-button");
 const musicaInput = document.querySelector("#alternar-musica");
 const startBtn = document.getElementById("start-pause");
+const startPauseBtn = document.querySelector("#start-pause span");
+const startPauseIcon = document.querySelector("#start-pause .app__card-primary-butto-icon");
+const timer = document.getElementById("timer");
 
 let musica = new Audio();
 musica.loop = true;
 
-const audioPlay = new Audio('/sons/play.wav');
-const audioPausa = new Audio('/sons/pause.mp3');
-const audioFim = new Audio('./sons/beep.mp3')
+const audioPlay = new Audio("/sons/play.wav");
+const audioPausa = new Audio("/sons/pause.mp3");
+const audioFim = new Audio("./sons/beep.mp3");
 
-let tempoDecorrido = 10;
+let tempoDecorrido = 1500;
 let intervalo = null;
+
+focoBtn.addEventListener("click", () => {
+    tempoDecorrido = 1500;
+    alterarContexto("foco", "foco.png");
+    focoBtn.classList.add("active");
+});
+
+curtoBtn.addEventListener("click", () => {
+    tempoDecorrido = 300;
+    alterarContexto("descanso-curto", "descanso-curto.png");
+    curtoBtn.classList.add("active");
+});
+
+longoBtn.addEventListener("click", () => {
+    tempoDecorrido = 900;
+    alterarContexto("descanso-longo", "descanso-longo.png");
+    longoBtn.classList.add("active");
+});
+
+function alterarContexto(contexto, imagem) {
+    mostrarTempo();
+    botoes.forEach(function (contexto) {
+        contexto.classList.remove("active");
+    });
+    html.setAttribute("data-contexto", contexto);
+    foto.setAttribute("src", `/imagens/${imagem}`);
+
+    switch (contexto) {
+        case "foco":
+            musica.pause();
+            musicaInput.checked = false;
+            titulo.innerHTML = `Otimize sua produtividade,<br/>
+            <strong class="app__title-strong">mergulhe no que importa.</strong>`;
+            break;
+        case "descanso-curto":
+            musica.pause();
+            musicaInput.checked = false;
+            titulo.innerHTML = `Que tal dar uma respirada?<br/>
+            <strong class="app__title-strong">Faça uma pausa curta!</strong>`;
+            break;
+        case "descanso-longo":
+            musica.pause();
+            musicaInput.checked = false;
+            titulo.innerHTML = `Hora de voltar à superfície<br/>
+            <strong class="app__title-strong">Faça uma pausa longa.</strong>`;
+            break;
+        default:
+            break;
+    }
+}
 
 /*
 play(): inicia a reprodução do áudio;
@@ -43,61 +97,6 @@ musicaInput.addEventListener("change", () => {
     } else musica.pause();
 });
 
-//Botão de Iniciar e Pausar Timer;
-const StartBtn = document.querySelector(".app__card-primary-button");
-//O elemento HTML em que irá aparecer o temporizador;
-let timer = document.querySelector("#app__card-timer");
-//O elemento HTML no qual as imagens de cada contexto irão trocar de posição;
-let foto = document.querySelector(".app__image");
-//O elemento HTML onde as frases de cada contexto irão trocar de posição.
-let texto = document.querySelector(".app__title");
-
-const timerFoco = 1500;
-const timerCurto = 300;
-const timerLongo = 900;
-
-focoBtn.addEventListener("click", () => {
-    alterarContexto("foco", "foco.png");
-    focoBtn.classList.add("active");
-});
-
-curtoBtn.addEventListener("click", () => {
-    alterarContexto("descanso-curto", "descanso-curto.png");
-    curtoBtn.classList.add("active");
-});
-
-longoBtn.addEventListener("click", () => {
-    alterarContexto("descanso-longo", "descanso-longo.png");
-    longoBtn.classList.add("active");
-});
-
-function alterarContexto(contexto, imagem) {
-    html.setAttribute("data-contexto", contexto);
-    foto.setAttribute("src", `/imagens/${imagem}`);
-
-    botoes.forEach(function (contexto) {
-        contexto.classList.remove("active");
-    });
-
-    switch (contexto) {
-        case "foco":
-            titulo.innerHTML = `Otimize sua produtividade,<br/>
-            <strong class="app__title-strong">mergulhe no que importa.</strong>`;
-            break;
-        case "descanso-curto":
-            titulo.innerHTML = `Que tal dar uma respirada?<br/>
-            <strong class="app__title-strong">Faça uma pausa curta!</strong>`;
-            break;
-        case "descanso-longo":
-            titulo.innerHTML = `Hora de voltar à superfície<br/>
-            <strong class="app__title-strong">Faça uma pausa longa.</strong>`;
-            break;
-        default:
-            break;
-    }
-}
-
-
 startBtn.addEventListener("click", iniciarPausar);
 
 const contagemRegressiva = () => {
@@ -105,12 +104,12 @@ const contagemRegressiva = () => {
         audioFim.volume = 0.5;
         audioFim.currentTime = 3;
         audioFim.play();
-        zerar();
         console.log(`Temporizador: ${tempoDecorrido}`);
+        zerar();
         return;
     }
-    console.log(`Temporizador: ${tempoDecorrido}`);
     tempoDecorrido -= 1;
+    mostrarTempo();
 };
 
 function iniciarPausar() {
@@ -122,9 +121,21 @@ function iniciarPausar() {
     }
     audioPlay.play();
     intervalo = setInterval(contagemRegressiva, 1000);
+    startPauseBtn.textContent = "Pausar";
+    startPauseIcon.setAttribute("src", "/imagens/pause.png");
 }
 
 function zerar() {
     clearInterval(intervalo);
+    startPauseBtn.textContent = "Começar";
+    startPauseIcon.setAttribute("src", "/imagens/play_arrow.png");
     intervalo = null;
 }
+
+function mostrarTempo() {
+    const tempo = new Date(tempoDecorrido * 1000);
+    const tempoFormat = tempo.toLocaleTimeString("pt-br", { minute: "2-digit", second: "2-digit" });
+    timer.innerHTML = `${tempoFormat}`;
+}
+
+mostrarTempo();
